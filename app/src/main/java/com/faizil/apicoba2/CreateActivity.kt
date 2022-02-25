c6d7d3b7ba145bd22e8445ebb25b1e06a0907b60
f4pro <faizilputra7@gmail.com>
2022-02-25 18:54:41 +0700

    Coba

--- app/src/main/java/com/faizil/apicoba2/CreateActivity.kt
+++ app/src/main/java/com/faizil/apicoba2/CreateActivity.kt
@@ -2,11 +2,19 @@ package com.faizil.apicoba2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
 
 class CreateActivity : AppCompatActivity() {
+    private lateinit var binding: InputBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
-        setTitle("Input")
     }
+/*
+    private fun createPos(){
+        RetrofitClient.instance.createPost(
+            name = name
+        )
+    }
+*/
 }
