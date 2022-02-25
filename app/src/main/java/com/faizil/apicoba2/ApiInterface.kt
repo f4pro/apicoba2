72db8f93c0ade43e637be6af15bad55afee466c3
f4pro <faizilputra7@gmail.com>
2022-02-25 10:30:44 +0700

    Coba 1

--- app/src/main/java/com/faizil/apicoba2/ApiInterface.kt
+++ app/src/main/java/com/faizil/apicoba2/ApiInterface.kt
@@ -1,9 +1,27 @@
 package com.faizil.apicoba2
 
 import retrofit2.Call
-import retrofit2.http.GET
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.HttpException
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-interface ApiInterface {
-    @GET("pegawai")
-    fun getData(): Call<List<ListDataItem>>
+class ApiInterface {
+    companion object{
+        val baseUrl = "https://61601920faa03600179fb8d2.mockapi.io/pegawai/"
+        fun getRetroInstance(): Retrofit{
+
+            val logging = HttpLoggingInterceptor()
+            logging.level = (HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+            client.addInterceptor(logging)
+
+            return Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(client.build())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
 }
